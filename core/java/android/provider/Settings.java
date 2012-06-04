7e0d2a2ef39d7793798e6fa7673c1a77e16d8931
Alex Avance <aravance@gmail.com>
2012-06-03 20:37:05 -0700

    Add an option to change the device hostname (1/2)
    This adds an option to modify the device hostname used
    in ip resolution. This is useful when connecting to the
    android device in a dynamic dhcp environment.
    
    Change-Id: I44e4771f4415a32fe66867d4dd90f1ad138341e1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11138,6 +11138,11 @@ public final class Settings {
         public static final String ADB_NOTIFY = "adb_notify";
 
         /**
+         * @hide
+         */
+        public static final String DEVICE_HOSTNAME = "device_hostname";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
