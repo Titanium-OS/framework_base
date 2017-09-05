1ba1010ad70c3f3ccaae2340d1c10c274c802c7f
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:34:10 +0800

    Incall vibration options [1/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)
    
    idoybh: Adapted to A11's settings backup
    Change-Id: I3d7a23b9608cd25d4759b79d3157963bfb18254f

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -90,5 +90,8 @@ public class SystemSettings {
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.PROXIMITY_ON_WAKE,
+        Settings.System.VIBRATE_ON_CONNECT,
+        Settings.System.VIBRATE_ON_CALLWAITING,
+        Settings.System.VIBRATE_ON_DISCONNECT,
     };
 }
