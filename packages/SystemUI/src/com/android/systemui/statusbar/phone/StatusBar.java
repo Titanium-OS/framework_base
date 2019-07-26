60ef9e0fd7a9e38a12e2fe7417522d3260a8de42
ezio84 <brabus84@gmail.com>
2019-07-26 12:45:22 +0300

    Less boring heads up option [1/2]
    allow to show heads up only for dialer and messaging
    app when they are not in foreground
    
    AICPfied: disabled by default
    
    Less annoying heads up: code fixes and improvements
    
    fix Ambient Display not working when disabling heads up
    remove the foreground app check, it's good to show heads up
    when Dialer is in foreground instead of incallui
    
    Change-Id: I08b035f5ba17a7964dc0d13385afb7d52fbfb395
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2163,6 +2163,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LESS_BORING_HEADS_UP),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -2198,6 +2201,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setPulseOnNewTracks();
             updateChargingAnimation();
             setScreenBrightnessMode();
+            setUseLessBoringHeadsUp();
         }
     }
 
@@ -2263,6 +2267,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         mBrightnessControl = Settings.System.getIntForUser(
             mContext.getContentResolver(), Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0,
             UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setUseLessBoringHeadsUp() {
+        boolean lessBoringHeadsUp = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LESS_BORING_HEADS_UP, 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNotificationInterruptStateProvider.setUseLessBoringHeadsUp(lessBoringHeadsUp);
     }
 
     /**
