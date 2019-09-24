dde7ca1749483fe682df53d972fd0527535c3c47
Marko Man <darkobas@gmail.com>
2019-09-24 14:46:01 +0200

    base: double tap to sleep on statusbar [1/2]
    arrow-11.0 edits:
    - Adapt code to refactored statusbar controllers.
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -85,5 +85,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         // TitaniumOS Additions
+        Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
     };
 }
