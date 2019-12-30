53b4c78dd32cd71eea56cebb2c22f61e6ed40973
DennySPB <dennyspb@gmail.com>
2019-12-30 16:35:34 +0300

    fonts: Nokia Pure and Fifa2018 [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -108,6 +108,8 @@ LOCAL_REQUIRED_MODULES := \
         FontLinotte \
 	FontOneplusSlateSource \
 	FontSamsungOneSourceOverlay \
+	FontNokiaPureSourceOverlay \
+	FontFifa2018SourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
