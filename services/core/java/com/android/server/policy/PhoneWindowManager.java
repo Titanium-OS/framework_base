5b753db89ea859b73576e4c104f4dbfd53e7c196
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-24 17:44:15 +0200

    Add screenshot type option for Longshot [1/2]
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -350,6 +350,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final String ACTION_TORCH_OFF =
             "com.android.server.policy.PhoneWindowManager.ACTION_TORCH_OFF";
 
+    private int sshotType;
+
     /**
      * Keyguard stuff
      */
@@ -927,6 +929,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.TORCH_LONG_PRESS_POWER_TIMEOUT), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREENSHOT_TYPE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1562,8 +1567,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             + SCREENSHOT_CHORD_DEBOUNCE_DELAY_MILLIS) {
                 mScreenshotChordVolumeDownKeyConsumed = true;
                 cancelPendingPowerKeyAction();
-                if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT) == 1) {
+                if (sshotType == 1) {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_SELECTED_REGION);
                 } else {
                     mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
@@ -1658,9 +1662,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         @Override
         public void run() {
-            if(!mPocketLockShowing) {
+            if (sshotType == 2) {
                 boolean dockMinimized = mWindowManagerInternal.isMinimizedDock();
-                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                if (!mPocketLockShowing) {
+                    mDefaultDisplayPolicy.takeScreenshot(mScreenshotType, dockMinimized);
+                }
+            } else {
+                mDefaultDisplayPolicy.takeScreenshot(mScreenshotType);
             }
         }
     }
@@ -2440,6 +2448,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (updateRotation) {
             updateRotation(true);
         }
+                sshotType = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_TYPE, 0, UserHandle.USER_CURRENT);
     }
 
     private void updateWakeGestureListenerLp() {
