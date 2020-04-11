PixelExperience/frameworks_base

framework: Implement light for popup camera
Change-Id: I2ff7729ceb72303071b91f7e76a915baf33ea431

core/java/com/android/internal/util/custom/popupcamera/PopUpCameraUtils.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2019 The Pixel Experience Project
+* Copyright (C) 2020 The Pixel Experience Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,6 +15,31 @@
 */
 package com.android.internal.util.custom.popupcamera;
 
+import android.content.Context;
+import android.provider.Settings;
+import android.os.UserHandle;
+
 public class PopUpCameraUtils {
-    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION = "org.pixelexperience.device.MANAGE_POPUP_CAMERA_SERVICE";
+    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION =
+            "org.pixelexperience.device.MANAGE_POPUP_CAMERA_SERVICE";
+
+    public static boolean isLedEnabled(Context context) {
+        boolean supportsLight = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_popUpNotificationLight);
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
+    public static void setLedEnabled(Context context, boolean enabled) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, enabled ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
+
+    public static void blockBatteryLed(Context context, boolean blocked) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, blocked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
 }

services/core/java/com/android/server/BatteryService.java
@@ -83,6 +83,8 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
+
 /**
  * <p>BatteryService monitors the charging status, and charge level of the device
  * battery.  When these values change this service broadcasts the new values
@@ -269,6 +271,7 @@ public final class BatteryService extends SystemService {
                 updateBatteryWarningLevelLocked();
             }
         } else if (phase == PHASE_BOOT_COMPLETED) {
+            PopUpCameraUtils.lockBatteryLed(mContext, false);
             SettingsObserver mObserver = new SettingsObserver(new Handler());
             mObserver.observe();
         }
@@ -288,6 +291,9 @@ public final class BatteryService extends SystemService {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BATTERY_LIGHT_ENABLED),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -299,7 +305,9 @@ public final class BatteryService extends SystemService {
         public void update() {
             ContentResolver resolver = mContext.getContentResolver();
             Resources res = mContext.getResources();
-            mBatteryLightEnabled = Settings.System.getInt(resolver,
+            boolean batteryLightTempBlocked = Settings.System.getInt(resolver,
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, 0) == 1;
+            mBatteryLightEnabled = !batteryLightTempBlocked && Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ENABLED, mContext.getResources().getBoolean(
                         com.android.internal.R.bool.config_intrusiveBatteryLed) ? 1 : 0) == 1;
             updateLed();

Titanium-OS/framework_base

(cherry picked from commit 4b5949199288d07bc628bce0a1705ef2462d3d7e)
Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

core/java/com/android/internal/util/custom/popupcamera/PopUpCameraUtils.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2019 The Pixel Experience Project
+* Copyright (C) 2020 The Pixel Experience Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,6 +15,34 @@
 */
 package com.android.internal.util.custom.popupcamera;
 
+import android.content.Context;
+import android.provider.Settings;
+import android.os.UserHandle;
+
 public class PopUpCameraUtils {
-    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION = "com.titanium.device.MANAGE_POPUP_CAMERA_SERVICE";
+    public static String MANAGE_POPUP_CAMERA_SERVICE_PERMISSION =
+            "com.titanium.device.MANAGE_POPUP_CAMERA_SERVICE";
+
+    public static boolean supportsLed(Context context) {
+        return context.getResources().getBoolean(
+                com.android.internal.R.bool.config_popUpNotificationLight);
+    }
+
+    public static boolean isLedEnabled(Context context) {
+        return Settings.System.getIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+    }
+
+    public static void setLedEnabled(Context context, boolean enabled) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_LED_ENABLED, enabled ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
+
+    public static void blockBatteryLed(Context context, boolean blocked) {
+        Settings.System.putIntForUser(context.getContentResolver(),
+                Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, blocked ? 1 : 0,
+                UserHandle.USER_CURRENT);
+    }
 }

services/core/java/com/android/server/BatteryService.java
@@ -86,6 +86,8 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.android.internal.util.custom.popupcamera.PopUpCameraUtils;
+
 /**
  * <p>BatteryService monitors the charging status, and charge level of the device
  * battery.  When these values change this service broadcasts the new values
@@ -285,6 +287,7 @@ public final class BatteryService extends SystemService {
                 updateBatteryWarningLevelLocked();
             }
         } else if (phase == PHASE_BOOT_COMPLETED) {
+            PopUpCameraUtils.blockBatteryLed(mContext, false);
             SettingsObserver mObserver = new SettingsObserver(new Handler());
             mObserver.observe();
         }
@@ -329,6 +332,9 @@ public final class BatteryService extends SystemService {
                         Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR),
                         false, this, UserHandle.USER_ALL);
             }
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -342,7 +348,9 @@ public final class BatteryService extends SystemService {
             Resources res = mContext.getResources();
 
             // Battery light enabled
-            mLightEnabled = Settings.System.getInt(resolver,
+            boolean batteryLightTempBlocked = Settings.System.getInt(resolver,
+                    Settings.System.POPUP_CAMERA_BATTERY_LED_BLOCKED, 0) == 1;
+            mLightEnabled = !batteryLightTempBlocked && Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ENABLED, 1) != 0;
             mAllowBatteryLightOnDnd = Settings.System.getInt(resolver,
                     Settings.System.BATTERY_LIGHT_ALLOW_ON_DND, 0) == 1;