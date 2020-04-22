30ac537034f469bc76cb99c5e9f3346ad28638d0
Anushek Prasal <anushekprasal@gmail.com>
2020-04-22 21:25:02 +0530

    SystemUI: Add OP Energy FOD animation from OnePlus 8 Pro [1/2]
    Energy animation extracted from Oxygen OS 10.5.4
    
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5766,7 +5766,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_ANIM_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 17);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 18);
 
         /**
          * Gaming mode master switch
