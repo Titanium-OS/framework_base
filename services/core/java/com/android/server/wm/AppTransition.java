boulzordev/android_frameworks_base

AppTransitions: Remove hackery for controls duration
* Use customseekbarpreference in settings.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

services/core/java/com/android/server/wm/AppTransition.java
@@ -2238,8 +2238,7 @@ public class AppTransition implements Dump {
             mActivityAnimations[i] = Settings.System.getIntForUser(resolver,
                 Settings.System.ACTIVITY_ANIMATION_CONTROLS[i], 0, UserHandle.USER_CURRENT);
         }
-        int temp = Settings.System.getIntForUser(resolver, Settings.System.ANIMATION_CONTROLS_DURATION,
+        mAnimationDuration = Settings.System.getIntForUser(resolver, Settings.System.ANIMATION_CONTROLS_DURATION,
                     0, UserHandle.USER_CURRENT);
-        mAnimationDuration = temp * 15;
     }
 }

Titanium-OS/framework_base

services/core/java/com/android/server/wm/AppTransition.java
@@ -2479,8 +2479,7 @@ public class AppTransition implements Dump {
             mActivityAnimations[i] = Settings.Global.getInt(resolver,
                 Settings.Global.ACTIVITY_ANIMATION_CONTROLS[i], 0);
         }
-        int temp = Settings.Global.getInt(resolver, Settings.Global.ANIMATION_CONTROLS_DURATION,
+        mAnimationDuration = Settings.Global.getInt(resolver, Settings.Global.ANIMATION_CONTROLS_DURATION,
                     0);
-        mAnimationDuration = temp * 15;
     }
 }